9d1f9b22be8887d1ada24a8ce9a86f2b22d53f4c
DoyeonKim <vlxjdus@gmail.com>
2019-10-26 10:01:04 +0900

    BeaconTest2
    버튼이벤트리스너 정리 + 블루투스&비콘 함수 추상화

--- app/src/main/java/com/example/capstondesign_team_cs/Rounding_DActivity.java
+++ app/src/main/java/com/example/capstondesign_team_cs/Rounding_DActivity.java
@@ -3,17 +3,32 @@ package com.example.capstondesign_team_cs;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.minew.beacon.BluetoothState;
+import com.minew.beacon.MinewBeacon;
+import com.minew.beacon.MinewBeaconManager;
+import com.minew.beacon.MinewBeaconManagerListener;
+
+import java.util.List;
+import java.util.UUID;
+
 public class Rounding_DActivity extends AppCompatActivity {
+    BtnOnClickListener onClickListener;
+    BluetoothAdapter mBluetoothAdapter;
+    private MinewBeaconManager mMinewBeaconManager;
+    private boolean isScanning;
+    final static int BT_REQUEST_ENABLE = 1;
+    //final static UUID BT_UUID = UUID.fromString("E2C56D85-DFFB-48D2-B060-DOF5A");
+
     Button btnStartRounding, btnCancelRounding;
     TextView txtInfoRounding;
     int nSelectReason = -1;
@@ -22,69 +37,158 @@ public class Rounding_DActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rounding__d);
+
+        onClickListener = new BtnOnClickListener();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btnStartRounding = findViewById(R.id.btnStartRounding);
         btnCancelRounding = findViewById(R.id.btnCancelRounding);
         txtInfoRounding = findViewById(R.id.txtInfoRounding);
 
         btnCancelRounding.setEnabled(false);
 
-        btnStartRounding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Rounding_DActivity.this,
-                        android.R.style.Theme_DeviceDefault_Light_Dialog);
-                alertDialogBuilder.setMessage("회진을 시작하시겠습니까?");
-                alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        txtInfoRounding.setText("회진 중");
-                        Toast.makeText(Rounding_DActivity.this,"회진을 시작합니다.", Toast.LENGTH_LONG).show();
-                        btnStartRounding.setEnabled(false);
-                        btnCancelRounding.setEnabled(true);
-                    }
-                });
-                alertDialogBuilder.setNegativeButton("아니오", null);
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-            }
-        });
+        btnStartRounding.setOnClickListener(onClickListener);
+        btnCancelRounding.setOnClickListener(onClickListener);
+        initManager();
+        initBeaconListener();
+        testSCanningBLE();
 
-        final String[] cancelReasons = {"회진이 끝났습니다.", "긴급수술로 인해 회진을 취소합니다."};
-        btnCancelRounding.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+    private void initManager() {
+        mMinewBeaconManager = MinewBeaconManager.getInstance(this);
+    }
+
+    private  void initBeaconListener() {
+        mMinewBeaconManager.setDeviceManagerDelegateListener(new MinewBeaconManagerListener() {
             @Override
-            public void onClick(View v) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(Rounding_DActivity.this);
-                builder.setTitle("회진 취소 사유를 선택하세요")
-                        .setSingleChoiceItems(cancelReasons, -1, new DialogInterface.OnClickListener() {
-                        @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.d("which = ", Integer.toString(which));
-                                nSelectReason = which;
-                            }
-                        })
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.d("nSelection = ", Integer.toString(nSelectReason));
-                                //which = nSelectReason;
-                                if(nSelectReason >= 0) {
-                                    txtInfoRounding.setText("회진시간이 아닙니다.");
-                                    Toast.makeText(getApplicationContext(),
-                                            cancelReasons[nSelectReason], Toast.LENGTH_LONG).show();
-                                    btnStartRounding.setEnabled(true);
-                                    btnCancelRounding.setEnabled(false);
-                                    nSelectReason = -1;
-                                }
-                                else {
-                                    Toast.makeText(Rounding_DActivity.this,
-                                            "이유를 선택하세요.", Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        })
-                        .setNeutralButton("취소", null);
-                builder.create();
-                builder.show();
+            public void onAppearBeacons(List<MinewBeacon> list) {
+
+            }
+
+            @Override
+            public void onDisappearBeacons(List<MinewBeacon> list) {
+
+            }
+
+            @Override
+            public void onRangeBeacons(List<MinewBeacon> list) {
+
+            }
+
+            @Override
+            public void onUpdateState(BluetoothState bluetoothState) {
+
             }
         });
+    }
+
+    private void checkBluetooth() {
+        BluetoothState bluetoothState = mMinewBeaconManager.checkBluetoothState();
+        switch (bluetoothState) {
+            case BluetoothStateNotSupported:
+                Toast.makeText(this, "Not Support BLE", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            case BluetoothStatePowerOff:
+                //showBLEDialog();
+                break;
+            case BluetoothStatePowerOn:
+                break;
+        }
+    }
+
+    private void testSCanningBLE() {
+        if (mMinewBeaconManager != null) {
+            checkBluetooth();
+        }
+        if (isScanning) {
+            isScanning = false;
+            if (mMinewBeaconManager != null) {
+                mMinewBeaconManager.stopScan();
+            }
+        } else {
+            isScanning = true;
+            try {
+                mMinewBeaconManager.startScan();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void bluetoothOn() {
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (mBluetoothAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "블루투스가 이미 활성화 되어 있습니다.", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "블루투스가 활성화 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+                Intent intentBluetoothEnable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intentBluetoothEnable, BT_REQUEST_ENABLE);
+            }
+        }
+    }
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnStartRounding :
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Rounding_DActivity.this,
+                            android.R.style.Theme_DeviceDefault_Light_Dialog);
+                    alertDialogBuilder.setMessage("회진을 시작하시겠습니까?");
+                    alertDialogBuilder.setPositiveButton("네", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            bluetoothOn();
+                            txtInfoRounding.setText("회진 중");
+                            Toast.makeText(Rounding_DActivity.this,"회진을 시작합니다.", Toast.LENGTH_LONG).show();
+                            btnStartRounding.setEnabled(false);
+                            btnCancelRounding.setEnabled(true);
+                        }
+                    });
+                    alertDialogBuilder.setNegativeButton("아니오", null);
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+                    break;
+                case R.id.btnCancelRounding :
+                    final String[] cancelReasons = {"회진이 끝났습니다.", "긴급수술로 인해 회진을 취소합니다."};
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Rounding_DActivity.this);
+                    builder.setTitle("회진 취소 사유를 선택하세요")
+                            .setSingleChoiceItems(cancelReasons, -1, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Log.d("which = ", Integer.toString(which));
+                                    nSelectReason = which;
+                                }
+                            })
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Log.d("nSelection = ", Integer.toString(nSelectReason));
+                                    if(nSelectReason >= 0) {
+                                        txtInfoRounding.setText("회진시간이 아닙니다.");
+                                        Toast.makeText(getApplicationContext(),
+                                                cancelReasons[nSelectReason], Toast.LENGTH_LONG).show();
+                                        btnStartRounding.setEnabled(true);
+                                        btnCancelRounding.setEnabled(false);
+                                        nSelectReason = -1;
+                                    }
+                                    else {
+                                        Toast.makeText(Rounding_DActivity.this,
+                                                "이유를 선택하세요.", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            })
+                            .setNeutralButton("취소", null);
+                    builder.create();
+                    builder.show();
+                    break;
+            }
+        }
     }
 }
