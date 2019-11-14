bd4708fd4158157239eac11998d63832f2fdfee4
DoyeonKim <vlxjdus@gmail.com>
2019-11-14 15:08:08 +0900

    ChatActivity 오류 수정
    이름 수정 후 오류 해결 / 이유는 모름

---
+++ app/src/main/java/com/example/capstondesign_team_cs/ChatMainActivity.java
@@ -0,0 +1,89 @@
+package com.example.capstondesign_team_cs;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChatMainActivity extends AppCompatActivity {
+
+    private EditText user_chat, user_edit;
+    private Button user_next;
+    private ListView chat_list;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = database.getReference("message");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_main);
+
+        user_chat = (EditText) findViewById(R.id.user_chat);
+        user_edit = (EditText) findViewById(R.id.user_edit);
+        user_next =  (Button) findViewById(R.id.user_next);
+        chat_list = (ListView) findViewById(R.id.chat_list);
+
+        user_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user_edit.getText().toString().equals("") || user_chat.getText().toString().equals(""))
+                    return;
+
+                Intent intent = new Intent(ChatMainActivity.this, ChattingActivity.class);
+                intent.putExtra("chatName", user_chat.getText().toString());
+                intent.putExtra("userName", user_edit.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        showChatList();
+    }
+
+    private void showChatList() {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        chat_list.setAdapter(adapter);
+
+        databaseReference.child("chat").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
