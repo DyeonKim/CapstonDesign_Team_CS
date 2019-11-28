a2357ca6f2f9793c4f56b8c6627375fe53be518f
DoyeonKim <vlxjdus@gmail.com>
2019-11-28 19:10:45 +0900

    회원가입 6차 수정
    코드상  오류는 없으나 db 확인을 똑바로 하지 않음

--- app/src/main/java/com/example/capstondesign_team_cs/UserInfo.java
+++ app/src/main/java/com/example/capstondesign_team_cs/UserInfo.java
@@ -1,9 +1,35 @@
 package com.example.capstondesign_team_cs;
 
 public class UserInfo {
+    Boolean state;
     String name;
     String email;
-    String password;
     String phone;
-    Boolean state;
+
+    public UserInfo() {
+
+    }
+
+    public void setUserInfo(Boolean state, String name, String email, String phone) {
+        this. state = state;
+        this. name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Boolean getUserState() {
+        return state;
+    }
+
+    public String getUserName() {
+        return name;
+    }
+
+    public String getUserEmail() {
+        return email;
+    }
+
+    public String getUserPhone() {
+        return phone;
+    }
 }
